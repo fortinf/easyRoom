77b891cab54e6847419363dcf30c40a19d33a652
Stephanie <slorant22@gmail.com>
2017-03-15 15:28:20 +0100

    maj de la vue salle

---
+++ src/EasyRoom/AppBundle/Resources/public/js/Salle/salle.js
@@ -0,0 +1,9 @@
+
+  $(document).ready(function() {
+    $('input#input_text, textarea#description-salle').characterCounter();
+  });
+
+  $('#description-salle').val('');
+  $('#textarea1').trigger('');
+        
+        
